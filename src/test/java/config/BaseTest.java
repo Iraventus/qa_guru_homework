29a896e2b0cbb63f05b168d9c6b21c7989914a23
Nick Tokarev <tokarev_ns>
2021-02-28 17:14:58 +0300

    add tlg bot

--- src/test/java/config/BaseTest.java
+++ src/test/java/config/BaseTest.java
@@ -1,7 +1,6 @@
 package config;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
